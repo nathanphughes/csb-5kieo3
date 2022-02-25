2574234f4304d1a129ec78c592fc3e055b75bd5d
Code Sandbox <hello@codesandbox.io>
2022-02-25 17:28:51 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.9383a9bf08be5107d0684d3829505b8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4afef06f16f6a1d010a7bf3f38ba0603",
+    "url": "https://nathanphughes.github.io/csb-5kieo3/index.html"
+  },
+  {
+    "revision": "76209d92fb4553c2d38b",
+    "url": "https://nathanphughes.github.io/csb-5kieo3/static/css/main.591722c4.chunk.css"
+  },
+  {
+    "revision": "0c3da51a3863818278f9",
+    "url": "https://nathanphughes.github.io/csb-5kieo3/static/js/2.a0307df6.chunk.js"
+  },
+  {
+    "revision": "dc1e4aa35d9abe4b3bfcec092bcae0e1",
+    "url": "https://nathanphughes.github.io/csb-5kieo3/static/js/2.a0307df6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76209d92fb4553c2d38b",
+    "url": "https://nathanphughes.github.io/csb-5kieo3/static/js/main.33ed2af9.chunk.js"
+  },
+  {
+    "revision": "d37d75844039b1c86b4d",
+    "url": "https://nathanphughes.github.io/csb-5kieo3/static/js/runtime-main.c7ca25b2.js"
+  }
+]);
